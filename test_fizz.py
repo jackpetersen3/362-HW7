dede062555ab44681c2bebc0aa91b5d498c2e981
jackpetersen3 <peterja7@oregonstate.edu>
2021-05-31 11:37:17 -0700

    test slightly changed to capture stdout, code written for fizzbuzz with multiples of 3

--- test_fizz.py
+++ test_fizz.py
@@ -1,6 +1,8 @@
 import pytest
 import fizzBuzz
 
-def test_three:
+def test_three(capsys):
     for i in [3, 6, 9, 12]:
-        assert fizzbuzz.fizzbuzz(i) == "Fizz"
+        fizzBuzz.fizzbuzz()
+        out = capsys.readouterr().out.split('\n')[:-1]
+        assert out[i]== 'Fizz'
