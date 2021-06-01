672ab500241bd4d648fcd65d869c38690963e003
jackpetersen3 <peterja7@oregonstate.edu>
2021-05-31 19:26:24 -0700

    test to check if year is divisible by 100 and not 400. no additonal code is needed

--- test_leap.py
+++ test_leap.py
@@ -7,4 +7,7 @@ def test_four():
 def test_100_and_400():
     for i in [1600, 2000, 2400]:
         assert leap_year.leap_year(i) == "yes"
+def test_100_only():
+    for i in [1700, 2100, 2300]:
+        assert leap_year.leap_year(i) == "no"
     
