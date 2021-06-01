3c1b877a32bfed2ec88659be88ea9fcbda744212
jackpetersen3 <peterja7@oregonstate.edu>
2021-05-31 19:27:44 -0700

    test added for numbers not divisible by 4

--- test_leap.py
+++ test_leap.py
@@ -10,4 +10,7 @@ def test_100_and_400():
 def test_100_only():
     for i in [1700, 2100, 2300]:
         assert leap_year.leap_year(i) == "no"
+def test_not_by_4():
+    for i in [1777, 2133, 2301]:
+        assert leap_year.leap_year(i) == "no"
     
