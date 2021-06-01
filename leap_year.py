c1991bdb7bbe845d3a423709fbab1b7b4b79c311
jackpetersen3 <peterja7@oregonstate.edu>
2021-05-31 19:25:12 -0700

    code added to check for divisibility by 100 and 400

--- leap_year.py
+++ leap_year.py
@@ -1,3 +1,12 @@
 def leap_year(year):
     if  year % 4 == 0:
-        return "yes"
+        if year % 100 == 0:
+            if year % 400 == 0:
+                #print(year, "is a leap year\n")
+                return "yes"
+            else:
+                #print(year, "is not a leap year\n")
+                return "no"
+        else:
+            #print(year, "is a leap year\n")
+            return "yes"
--- test_leap.py
+++ test_leap.py
@@ -5,6 +5,6 @@ def test_four():
     for i in [2000, 2020, 2024]:
         assert leap_year.leap_year(i) == "yes"
 def test_100_and_400():
-    for i in [1600, 2000, 2400]
-    assert leap_year.leap_year(i) == "yes"
+    for i in [1600, 2000, 2400]:
+        assert leap_year.leap_year(i) == "yes"
     
