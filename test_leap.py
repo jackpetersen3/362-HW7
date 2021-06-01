debc6eb809275c67a181961e1d449b70cc8e0d1e
jackpetersen3 <peterja7@oregonstate.edu>
2021-05-31 19:24:19 -0700

    test that year is divisible by both 100 and 400

--- test_leap.py
+++ test_leap.py
@@ -4,4 +4,7 @@ import leap_year
 def test_four():
     for i in [2000, 2020, 2024]:
         assert leap_year.leap_year(i) == "yes"
+def test_100_and_400():
+    for i in [1600, 2000, 2400]
+    assert leap_year.leap_year(i) == "yes"
     
